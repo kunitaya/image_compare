35d18d7c8e731f0f9b995bd3e43e3b2b37d1a4de
kunitaya <tetsuya@kunitaya.info>
2017-11-29 23:07:58 +0900

    bugfix

--- image_compare.py
+++ image_compare.py
@@ -2,10 +2,11 @@ from skimage.measure import compare_ssim
 import imutils
 import cv2
 import os
+import sys
 
 header_A = 'after'
 header_B = 'before'
-header_out = 'compare'
+header_C = 'compare'
 
 def find_all_files(directory = os.path.abspath(__file__)):
     for root, dirs, files in os.walk(directory):
@@ -14,17 +15,13 @@ def find_all_files(directory = os.path.abspath(__file__)):
             yield os.path.join(root, file)
 
 def compare_image():
-    text_file = open(os.path.join(header_out, 'output.syslog'), 'a')
+    text_file = open(os.path.join(header_C, 'output.syslog'), 'a')
     for i in range(len(path)):
         fileA = os.path.join(os.path.dirname(os.path.abspath(__file__)), header_A, path[i])
         fileB = os.path.join(os.path.dirname(os.path.abspath(__file__)), header_B, path[i])
         imageA = cv2.imread(fileA, 0)
         imageB = cv2.imread(fileB, 0)
 
-        #grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
-        #grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
-
-        #(score, diff) = compare_ssim(grayA, grayB, full=True, multichannel=True)
         try:
             (score, diff) = compare_ssim(imageA, imageB, full=True, multichannel=True)
         except:
@@ -51,17 +48,17 @@ def compare_image():
                             + header_B + path[i] + '\\a.png, '
                             + "Similarity: %s" % percent + """%""" + "\n")
 
-        cv2.imwrite(os.path.join(os.path.join(header_out + path[i]), 'check-result-20171005233158.png'), imageA)
-        cv2.imwrite(os.path.join(os.path.join(header_out + path[i]), 'check-result-20171016144416.png'), imageB)
+        cv2.imwrite(os.path.join(os.path.join(header_C + path[i]), 'check-result-20171005233158.png'), imageA)
+        cv2.imwrite(os.path.join(os.path.join(header_C + path[i]), 'check-result-20171016144416.png'), imageB)
         cv2.waitKey(0)
         print(path[i])
     text_file.close()
 
 absdir = os.path.dirname(os.path.abspath(__file__))
 filesA = filter(lambda f:os.path.isfile(f), find_all_files(os.path.join(absdir, header_A)))
-mapA = map(lambda s:s.replace(os.path.join(absdir, header_A), ''),filesA)
+mapA = map(lambda s:s.replace(os.path.join(absdir, header_A) + os.sep, ''),filesA)
 filesB = filter(lambda f:os.path.isfile(f), find_all_files(os.path.join(absdir, header_B))):
-mapB = map(lambda s:s.replace(os.path.join(absdir, header_B), ''),filesB)
+mapB = map(lambda s:s.replace(os.path.join(absdir, header_B) + os.sep, ''),filesB)
 
 path = list(set(list(mapA)) & set(list(mapB)))
 
