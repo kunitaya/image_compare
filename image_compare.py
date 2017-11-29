4ffdf39b36eb00b010b815dd4c637e680b8ac97f
kunitaya <tetsuya@kunitaya.info>
2017-11-30 05:31:25 +0900

    performance tuning

--- image_compare.py
+++ image_compare.py
@@ -1,8 +1,18 @@
+# -*- coding: utf-8 -*-
 from skimage.measure import compare_ssim
 import imutils
 import cv2
 import os
 import sys
+
+# Logging
+from logging import getLogger, StreamHandler, DEBUG
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
 
 header_A = 'after'
 header_B = 'before'
@@ -14,59 +24,53 @@ def find_all_files(directory = os.path.abspath(__file__)):
         for file in files:
             yield os.path.join(root, file)
 
-def compare_image():
-    text_file = open(os.path.join(header_C, 'output.syslog'), 'a')
-    for i in range(len(path)):
-        fileA = os.path.join(os.path.dirname(os.path.abspath(__file__)), header_A, path[i])
-        fileB = os.path.join(os.path.dirname(os.path.abspath(__file__)), header_B, path[i])
-        pathC = os.path.dirname(os.path.join(os.path.dirname(os.path.abspath(__file__)), header_C, path[i]))
+def compare_image(str_path):
+    fileA = os.path.join(os.path.dirname(os.path.abspath(__file__)), header_A, str_path)
+    fileB = os.path.join(os.path.dirname(os.path.abspath(__file__)), header_B, str_path)
+    pathC = os.path.dirname(os.path.join(os.path.dirname(os.path.abspath(__file__)), header_C, str_path))
 
-        if not os.path.exists(pathC): os.makedirs(pathC)
+    if not os.path.exists(pathC): os.makedirs(pathC)
 
-        imageA = cv2.imread(fileA, 1)
-        imageB = cv2.imread(fileB, 1)
-        grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
-        grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+    imageA = cv2.imread(fileA, 1)
+    imageB = cv2.imread(fileB, 1)
+    grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+    grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
 
-        try:
-            (score, diff) = compare_ssim(grayA, grayB, full=True, multichannel=True)
-        except ValueError as e:
-            print(path[i] + ": ZeroDivisionError")
-            text_file.write(path[i] + ": ZeroDivisionError")
-            continue
-        except:
-            print(path[i] + ": " + "Unexpected error: ", sys.exc_info()[0])
-            continue
-
+    try:
+        (score, diff) = compare_ssim(grayA, grayB, full=True, multichannel=True)
         diff = (diff * 255).astype("uint8")
 
-        thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    except ValueError as e:
+        logger.debug(str_path + ": ZeroDivisionError")
+        return 0
+    except:
+        logger.debug(str_path + ": " + "Unexpected error: ", sys.exc_info()[0])
+        return 0
 
-        for c in cnts:
-            (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
-        if score < 0:
-            percent = ((score * 100) / 2) - 50
-            text_file.write(fileA + ', ' + fileB + ', ' + "Similarity: %s" % percent + """%""" + "\n")
+    for c in cnts:
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-        else:
-            percent = round(((score * 100) / 2) + 50, 2)
-            #fie = os.path.join(header_A + path[i] + '\\a.png\n', header_B + path[i] + '\\a.png\n')
-            #text_file.write(header_A + path[i] + '\\a.png, '
-            #                + header_B + path[i] + '\\a.png, '
-            #                + "Similarity: %s" % percent + """%""" + "\n")
-            text_file.write(fileA + ', ' + fileB + ', ' + "Similarity: %s" % percent + """%""" + "\n")
+    if score < 0:
+        percent = ((score * 100) / 2) - 50
+        logger.debug(fileA + ', ' + fileB + ', ' + "Similarity: {0}%".format(percent))
 
+    else:
+        percent = round(((score * 100) / 2) + 50, 2)
+        #fie = os.path.join(header_A + str_path + '\\a.png\n', header_B + str_path + '\\a.png\n')
+        #logfile.write(header_A + str_path + '\\a.png, '
+        #                + header_B + str_path + '\\a.png, '
+        #                + "Similarity: %s" % percent + """%""" + "\n")
+        logger.debug(fileA + ', ' + fileB + ', ' + "Similarity: {0}%".format(percent))
 
-        cv2.imwrite(os.path.join(pathC, header_A + '.png'), imageA)
-        cv2.imwrite(os.path.join(pathC, header_B + '.png'), imageB)
-        cv2.waitKey(0)
-        print(path[i])
-    text_file.close()
+    cv2.imwrite(os.path.join(pathC, header_A + '.png'), imageA)
+    cv2.imwrite(os.path.join(pathC, header_B + '.png'), imageB)
+    cv2.waitKey(0)
 
 absdir = os.path.dirname(os.path.abspath(__file__))
 filesA = filter(lambda f:os.path.isfile(f), find_all_files(os.path.join(absdir, header_A)))
@@ -76,5 +80,6 @@ mapB = map(lambda s:s.replace(os.path.join(absdir, header_B) + os.sep, ''),files
 
 path = list(set(list(mapA)) & set(list(mapB)))
 
-compare_image()
-print("Process Done")
+if __name__ == '__main__':
+    list(map(compare_image, path))
+    print("Process Done")
